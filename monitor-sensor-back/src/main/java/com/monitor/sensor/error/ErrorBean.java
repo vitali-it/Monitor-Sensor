508e2748fef2e4af2a6a2148569af0c90c82729c
Vitali <Vitali@DESKTOP-5FT89B7>
2020-05-07 06:53:27 +0300

    [28] Global error handler on the back

---
+++ monitor-sensor-back/src/main/java/com/monitor/sensor/error/ErrorBean.java
@@ -0,0 +1,23 @@
+package com.monitor.sensor.error;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+@Data
+public class ErrorBean {
+
+    private final String message;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private final LocalDateTime timestamp;
+
+    private final int status;
+}
