ff7b815d636b86f9935edfed7b498fc5b9dc2bf2
Vitali <Vitali@DESKTOP-5FT89B7>
2021-04-05 14:57:09 +0300

    [46] Int test

---
+++ monitor-sensor-back/src/test/java/com/monitor/sensor/controller/integration/SensorControllerIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.monitor.sensor.controller.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.monitor.sensor.controller.SensorController;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SensorControllerIntegrationTest {
+
+	@Autowired
+	private SensorController controller;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	public void shouldReturnUnauthorizedStatusForAll() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/sensors")).andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized());
+	}
+}
